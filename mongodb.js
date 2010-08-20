e2d04cf24f4cbbc58a6f32931cf5cafdb6b9940f
Jim Suchy <jsuchy@gmail.com>
2010-08-20 09:27:24 -0500

    initial commit

---
+++ mongodb.js
@@ -0,0 +1,43 @@
+var mongo = require('mongodb');
+var sys = require('sys');
+
+var db = new mongo.Db('node-mongo-8LU', new mongo.Server('localhost', 27017, {}), {});
+
+db.open(function(err, db) {
+  db.collection('records', function(err, collection) {
+    
+    // Remove all existing documents in collection
+    collection.remove(function(err, collection) {
+      
+      // Insert 3 records
+      for(var i = 0; i < 3; i++) {
+				user = {
+					"name": "user" + i,
+					"data": "some data"
+				}
+        collection.insert(doc);
+      }
+			
+			// Inspect records
+			collection.find(function(err,cursor){
+			  cursor.each(function(err,doc){
+				  if(doc != null){
+						sys.puts(sys.inspect(doc));
+				  }
+			  });
+			});
+			
+			// Count records
+			collection.count(function(err, count) {
+				sys.puts(count + " records.");
+			});
+			
+			db.close();
+    });
+  });
+});
+
+// mongo
+// use node-mongo-8LU
+// show collections
+// db.records.find();
---
+++ mongoose.js
@@ -0,0 +1,54 @@
+var mongoose = require('mongoose').Mongoose;
+var sys = require('sys');
+
+mongoose.model('Category', {
+
+    properties: ['name', 'updated_at'],
+
+    cast: {
+      age: Number,
+      'nested.path': String
+    },
+
+    indexes: ['first'],
+
+    setters: {
+        first: function(v){
+            return this.v.capitalize();
+        }
+    },
+
+    getters: {
+        full_name: function(){ 
+            return this.first + ' ' + this.last 
+        }
+    },
+
+    methods: {
+        save: function(fn){
+            this.updated_at = new Date();
+            this.__super__(fn);
+        }
+    },
+
+    static: {
+        findOldPeople: function(){
+            return this.find({age: { '$gt': 70 }});
+        }
+    }
+
+});
+
+var db = mongoose.connect('mongodb://localhost/mongoose_db');
+
+var User = db.model('User');
+
+var u = new User();
+u.name = 'John';
+u.save(function(){
+    sys.puts('Saved!');
+});
+
+var user = User.find({ name: 'john' });
+sys.puts(sys.inspect(user));
+
