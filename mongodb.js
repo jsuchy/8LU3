c46f77ab024e4068c0d2811a04605d8ca1a81ab6
Jim Suchy <jsuchy@gmail.com>
2010-08-20 23:20:24 -0500

    use original db and collection

--- mongodb.js
+++ mongodb.js
@@ -1,10 +1,10 @@
 var mongo = require('mongodb');
 var sys = require('sys');
 
-var db = new mongo.Db('node-mongo-examples', new mongo.Server('localhost', 27017, {}), {});
+var db = new mongo.Db('node-mongo-8LU', new mongo.Server('localhost', 27017, {}), {});
 
 db.open(function(err, db) {
-	db.collection('test', function(err, collection) {
+	db.collection('records', function(err, collection) {
 		
 		sys.puts(sys.inspect(collection));
     // Remove all existing documents in collection
